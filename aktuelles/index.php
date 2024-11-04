5ada8620f34a0ff97049c643b8e551ac9fe15ce9
max <maximilianfl@outlook.de>
2024-11-04 09:33:15 +0100

    m

---
+++ aktuelles/index.php
@@ -0,0 +1,35 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . "/config/index.php";
+
+// Create a connection to the database
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+
+$sessionid = $_COOKIE["session"];
+
+
+$u = isset($_GET['u']) ? intval($_GET['u']) : null;
+if($u == null){
+
+    $result = mysqli_execute_query($conn, "select * from users where session_id = ? ", [$sessionid]);
+    if ($result->num_rows > 0) {
+       while ($row = $result->fetch_assoc()) {
+            header("Location: ./?u=". $row["id"]);
+            exit;
+       }
+    }else{
+        header("Location: ../");
+        exit;
+    }
+
+
+
+
+}
+
+?>
--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db_ip = "localhost";
-$db_name = "u128392202_bigtime";
-$db_user = "u128392202_bigtime";
-$db_pass = "8838BigTime!!";
-
-
-?>
